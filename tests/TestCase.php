987b0fc72be51cb2bb385a79ef68e4bcda6844ae
Rafael I <rafael.indriago93@gmail.com>
2023-12-19 00:28:23 -0400

    Update base TestCase to use DatabaseMigrations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use DatabaseMigrations;
 }
