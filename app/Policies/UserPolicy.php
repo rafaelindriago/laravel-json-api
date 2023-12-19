8f41cb2ecc7d8315244efdb6dc87b46625a2e263
Rafael I <rafael.indriago93@gmail.com>
2023-12-19 01:23:34 -0400

    Update User policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -13,7 +13,7 @@ class UserPolicy
      */
     public function viewAny(?User $user): ?bool
     {
-        return null;
+        return true;
     }
 
     /**
@@ -21,7 +21,7 @@ class UserPolicy
      */
     public function view(?User $user, User $model): ?bool
     {
-        return null;
+        return true;
     }
 
     /**
@@ -29,7 +29,7 @@ class UserPolicy
      */
     public function create(?User $user): ?bool
     {
-        return null;
+        return true;
     }
 
     /**
@@ -37,7 +37,7 @@ class UserPolicy
      */
     public function update(?User $user, User $model): ?bool
     {
-        return null;
+        return true;
     }
 
     /**
@@ -45,7 +45,7 @@ class UserPolicy
      */
     public function delete(?User $user, User $model): ?bool
     {
-        return null;
+        return true;
     }
 
     /**
@@ -53,7 +53,7 @@ class UserPolicy
      */
     public function restore(?User $user, User $model): ?bool
     {
-        return null;
+        return true;
     }
 
     /**
@@ -61,6 +61,6 @@ class UserPolicy
      */
     public function forceDelete(?User $user, User $model): ?bool
     {
-        return null;
+        return true;
     }
 }
